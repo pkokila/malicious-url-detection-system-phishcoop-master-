1e07537784bff47cb3454711e125122dae199da4
pkokila <60467467+pkokila@users.noreply.github.com>
2021-02-22 21:42:10 +0530

    Add files via upload

---
+++ phishcoop-master/api.py
@@ -0,0 +1,50 @@
+import flask
+from flask import Flask, render_template, request
+from sklearn.externals import joblib
+import inputScript
+import regex
+
+import sys
+import logging
+
+
+app = Flask(__name__)
+
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return flask.render_template('home.html')
+
+@app.route('/about')
+def about():
+    return flask.render_template('about.html')
+
+@app.route('/predict', methods = ['POST'])
+def make_prediction():
+    classifier = joblib.load('rf_final.pkl')
+    if request.method=='POST':
+        url = request.form['url']
+        if not url:
+            return render_template('home.html', label = 'Please input url')
+        elif(not(regex.search(r'^(http|ftp)s?://', url))):
+            return render_template('home.html', label = 'Please input full url, for exp- https://google.com')
+        
+        
+        checkprediction = inputScript.main(url)
+        prediction = classifier.predict(checkprediction)
+
+        if prediction[0]==1 :
+            label = "WEBSITE  IS  NOT  LEGITIMATE"
+        elif prediction[0]==-1:
+            label ='WEBSITE  IS  LEGITIMATE'
+        
+        return render_template('home.html', label=label)
+        
+        
+if __name__ == '__main__':
+    classifier = joblib.load('rf_final.pkl')
+    app.run()
